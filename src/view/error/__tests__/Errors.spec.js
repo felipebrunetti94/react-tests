ca67e3876a819bb7f24371bacd71b05e8e6529be
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-04-28 23:17:55 -0300

    feat: add error component tests description

--- src/view/error/__tests__/Errors.spec.js
+++ src/view/error/__tests__/Errors.spec.js
@@ -1,26 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Errors from "../Errors";
 
 describe("View :: error :: Errors", () => {
-  describe("when receive an error with detail", () => {
-    it("render error list", () => {
-      render(<Errors error={{ details: ["ohno!", "nope!"] }} />);
-      expect(screen.getByTestId("errors")).toBeInTheDocument(/ohono!/);
-      expect(screen.getByTestId("errors")).toBeInTheDocument(/nope!/);
-    });
+  describe("quando recebe um erro com detalhes", () => {
+    it("renderiza lista de erros", () => {});
   });
 
-  describe("when receive an error with a message", () => {
-    it("render an error", () => {
-      render(<Errors error={{ message: "ohno!" }} />);
-      expect(screen.getByTestId("errors")).toBeInTheDocument(/ohono!/);
-    });
+  describe("quando recebe um erro com uma mensagem", () => {
+    it("renderiza um erro", () => {});
   });
 
-  describe("when receive an empty error", () => {
-    it("render nothing", () => {
-      const { container } = render(<Errors error={{}} />);
-      expect(container).toBeEmptyDOMElement();
-    });
+  describe("quando recebe um objeto vazio", () => {
+    it("renderiza nada", () => {});
   });
 });
