3d33a47d383bd842e5a5d97f4ebbecabcf4adbe5
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-04-28 23:19:42 -0300

    feat: respostas error

--- src/view/error/__tests__/Errors.spec.js
+++ src/view/error/__tests__/Errors.spec.js
@@ -3,14 +3,26 @@ import Errors from "../Errors";
 
 describe("View :: error :: Errors", () => {
   describe("quando recebe um erro com detalhes", () => {
-    it("renderiza lista de erros", () => {});
+    it("renderiza lista de erros", () => {
+      const { getByTestId } = render(
+        <Errors error={{ details: ["ohno!", "nope!"] }} />
+      );
+      expect(getByTestId("errors")).toBeInTheDocument(/ohono!/);
+      expect(getByTestId("errors")).toBeInTheDocument(/nope!/);
+    });
   });
 
   describe("quando recebe um erro com uma mensagem", () => {
-    it("renderiza um erro", () => {});
+    it("renderiza um erro", () => {
+      const { getByTestId } = render(<Errors error={{ message: "ohno!" }} />);
+      expect(getByTestId("errors")).toBeInTheDocument(/ohono!/);
+    });
   });
 
   describe("quando recebe um objeto vazio", () => {
-    it("renderiza nada", () => {});
+    it("renderiza nada", () => {
+      const { container } = render(<Errors error={{}} />);
+      expect(container).toBeEmptyDOMElement();
+    });
   });
 });
